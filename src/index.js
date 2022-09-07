989f7dfaf1774ea11b76857bf127f3e8d75a32b4
marvyCodes <olowomarve@gmail.com>
2022-09-07 20:41:52 +0100

    fixd index,js

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-    <App />
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
-  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
