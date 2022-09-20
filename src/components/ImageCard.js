1e58ca2eb8609d9b495ac62f575186fe2fa612b1
marvyCodes <olowomarve@gmail.com>
2022-09-20 22:09:38 +0100

    fix: bg-url-image

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,11 +3,12 @@ import React from "react";
 function ImageCard(props) {
     return (
         // eslint-disable-next-line 
-        <div className={"justify-self-center bg-[url('" +`${props.url}` + "')] w-[160px] h-[200px] rounded-lg  bg-cover"}>
-            <div className="ml-2 mt-36">
-                <h1 className="font-medium text-black">{props.name}</h1>
+        <div className="justify-self-center w-[160px] h-[200px] relative">
+            <div className="ml-2 mt-36 absolute">
+                <h1 className="font-medium text-[#FFFF]">{props.name}</h1>
                 <p className="text-sm font-small text-[#FFFF]">{props.area}</p>
             </div>
+            <img src={props.url} className=" w-[160px] h-[200px] rounded-lg" alt="img"/>
         </div>
     )
 }
