203a0d9b70357a9b670d2c4e5277156c19a15ac6
Tobi Aderibigbe <0xdegn@gmail.com>
2022-09-27 21:01:48 +0100

    Fixed Modal.js and ImageCard.js

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,28 +1,44 @@
 import React from "react";
-import { setProps, toggleModal} from '../store/toggle/toggleSlice';
-import { useDispatch } from 'react-redux';
+import { setProps, toggleModal } from "../store/toggle/toggleSlice";
+import { useDispatch } from "react-redux";
 
 function ImageCard(props) {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-    
-    const setModalParams = () => {
-        const imgUrl = props.url;
-        const name = props.name;
-        const area = props.area;
-        dispatch(setProps({ modalImageUrl: imgUrl, modalUserName: name, modalUserLocation: area }));
-        dispatch(toggleModal());
-    };
-
-    return (
-        <div className="justify-self-center w-[160px] h-[200px] relative z-40" onClick={setModalParams}>
-            <div className="ml-2 mt-36 absolute">
-                <h1 className="font-medium text-[#FFFF]">{props.name}</h1>
-                <p className="text-sm font-small text-[#FFFF]">{props.area}</p>
-            </div>
-            <img src={props.url} className=" w-[160px] h-[200px] rounded-lg" alt="img"/>
-        </div>
+  const setModalParams = () => {
+    const imgUrl = props.url;
+    const name = props.name;
+    const area = props.area;
+    dispatch(
+      setProps({
+        modalImageUrl: imgUrl,
+        modalUserName: name,
+        modalUserLocation: area,
+      })
     );
-};
+    dispatch(toggleModal());
+  };
+
+  return (
+    <div
+      className="justify-self-center w-[160px] h-[200px] relative z-40"
+      onClick={setModalParams}
+    >
+      <div className="ml-2 mt-36 absolute">
+        <h1 className="font-medium text-[#FFFF]">
+          {props.name === null ? "No name" : props.name}
+        </h1>
+        <p className="text-sm font-small text-[#FFFF]">
+          {props.area === null ? "Location not found" : props.area}
+        </p>
+      </div>
+      <img
+        src={props.url}
+        className=" w-[160px] h-[200px] rounded-lg"
+        alt="img"
+      />
+    </div>
+  );
+}
 
 export default ImageCard;
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setProps, toggleModal} from '../store/toggle/toggleSlice';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setProps, toggleModal } from "../store/toggle/toggleSlice";
 
 function Modal() {
-    const { modalProps, isModalOpen }= useSelector(state => state.toggle);
-    const dispatch = useDispatch();
+  const { modalProps, isModalOpen } = useSelector((state) => state.toggle);
+  const dispatch = useDispatch();
 
-    const clear = () => {
-        dispatch(toggleModal());
-        dispatch(setProps({ modalImageUrl: "", modalUserName: "", modalUserLocation: "" }));
-    }
-    return (
-        <div className={isModalOpen ? "block fixed rounded-lg bg-white left-0 right-0 top-20 mx-auto w-4/5 md:w-[600px] max-h-1/2 z-20" : "hidden"} onClick={clear}>
-            <img src={modalProps.modalImageUrl} alt="modal" className='w-full rounded-lg '/>
-            <div className='ml-6 mt-2 mb-3'>
-                <h1 className="font-medium text-black">{modalProps.modalUserName}</h1>
-                <p className="text-sm font-small text-black">{modalProps.modalUserLocation}</p>
-            </div>
-        </div>
+  const clear = () => {
+    dispatch(toggleModal());
+    dispatch(
+      setProps({ modalImageUrl: "", modalUserName: "", modalUserLocation: "" })
     );
-};
+  };
+  return (
+    <div
+      className={
+        isModalOpen
+          ? "block fixed rounded-lg bg-white left-2/4 top-2/4 -translate-y-1/2 -translate-x-1/2 mx-auto w-4/5 md:w-[600px] max-h-1/2 z-20 flex flex-col"
+          : "hidden"
+      }
+      onClick={clear}
+    >
+      <div>
+        <img
+          src={modalProps.modalImageUrl}
+          alt="modal"
+          className="w-full rounded-lg "
+        />
+        <div className="ml-6 mt-2 mb-3">
+          <h1 className="font-medium text-black">{modalProps.modalUserName === null ? "User not found" : modalProps.modalUserName}</h1>
+          <p className="text-sm font-small text-black">
+            {modalProps.modalUserLocation === null ? "Location not found" : modalProps.modalUserLocation}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Modal;
