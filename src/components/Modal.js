85c91bee41f8187c66eafdcc3780b9a64b338e7b
marvyCodes <olowomarve@gmail.com>
2022-09-27 20:04:11 +0100

    fixed modal height

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -11,7 +11,7 @@ function Modal() {
         dispatch(setProps({ modalImageUrl: "", modalUserName: "", modalUserLocation: "" }));
     }
     return (
-        <div className={isModalOpen ? "block fixed rounded-lg bg-white left-0 right-0 top-54 mx-auto w-4/5 md:w-[600px] max-h-1/2 z-20" : "hidden"} onClick={clear}>
+        <div className={isModalOpen ? "block fixed rounded-lg bg-white left-0 right-0 top-20 mx-auto w-4/5 md:w-[600px] max-h-1/2 z-20" : "hidden"} onClick={clear}>
             <img src={modalProps.modalImageUrl} alt="modal" className='w-full rounded-lg '/>
             <div className='ml-6 mt-2 mb-3'>
                 <h1 className="font-medium text-black">{modalProps.modalUserName}</h1>
